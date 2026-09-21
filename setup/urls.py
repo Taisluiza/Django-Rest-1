ccec8615b8d991b8b42a397b430409ce3b8bb548
TaisLuiza <taissalissousa@gmail.com>
2026-09-21 08:58:00 -0300

    Implementando a API, confirgurando o arqui ulrs.py da pasta setup para conseguindo visualizar pelo navegador

--- setup/urls.py
+++ setup/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from escola.views import estudantes
+from escola.views import EstudanteViewSet, CursoViewSet
+
+
+router = routers.DefaultRouter()
+router.register('estudantes', EstudanteViewSet, basename='Estudantes')
+router.register('cursos', CursoViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estudantes/', estudantes)
+    path('', include(router.urls))
 ]
